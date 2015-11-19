3779a9da3b9ea82e6ff5d52cde7be1d0b6dfcf05
mattgray <matthew.gray@mail.mcgill.ca>
2015-11-19 15:50:18 -0500

    Added function to format time for different units

--- src/Simulation/ShortestPath.java
+++ src/Simulation/ShortestPath.java
@@ -7,6 +7,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.concurrent.TimeUnit;
 
 public class ShortestPath {
 	public final double PROP_SPEED = 200000000.0; //m/s
@@ -16,7 +17,7 @@ public class ShortestPath {
 		Node[] shortestPath = getShortestPath(destNode);
 		System.out.println("\nShortest path from " + sourceNode.getName() + " to " + destNode.getName() + ":");
 		for(Node p: shortestPath){
-			System.out.print(p.getName() + "(" + p.getKey() + "), ");
+			System.out.print(p.getName() + "(" + p.getKey() + ") --> ");
 		}
 		System.out.println("\n");
 		sendPacket(shortestPath, message);
@@ -32,7 +33,7 @@ public class ShortestPath {
 			messageSizeBytes = message.length();
 		}
 		messageSizeBytes += messageOverheadBytes;
-		
+		double totalTime = 0.0;
 		for(int i=0; i < pathList.length-1; i++){
 			Node currentNode = pathList[i];
 			Node nextNode = pathList[i+1];
@@ -41,16 +42,29 @@ public class ShortestPath {
 			Edge nextEdge = getNextEdge(currentNode, nextNode);
 			double transmissionTime = ((float)messageSizeBytes)/nextEdge.getLink_speed();
 			double propagationTime = nextDistance / PROP_SPEED;
-			double totalTime = transmissionTime + propagationTime;
+			double thisLinkTime = transmissionTime + propagationTime;
+			totalTime += thisLinkTime;
 			try {
-			    Thread.sleep((long) (totalTime * 1000));                 //1000 milliseconds is one second.
+			    Thread.sleep((long) (thisLinkTime * 1000));                 //1000 milliseconds is one second.
 			} catch(InterruptedException ex) {
 			    Thread.currentThread().interrupt();
 			}
-			System.out.println("Message reached " + nextNode.getName() + 
-							   " in time " + totalTime + "s");
+			System.out.println(currentNode.getName() + " to " + nextNode.getName() +
+							   " took " + formatSeconds(thisLinkTime));
 		}
-		System.out.println("Final message received: " + message);
+		System.out.println("Final message received: " + message + " in time " + formatSeconds(totalTime));
+	}
+	
+	public String formatSeconds(double timeSeconds){
+		if(timeSeconds >= 0.1){
+			return String.format("%.3f",timeSeconds) + "s";
+		}else if(timeSeconds >= 0.0001){
+			return String.format("%.3f",(timeSeconds * 1000.0)) + "ms";
+		}else if(timeSeconds >= 0.0000001){
+			return String.format("%.3f",(timeSeconds * 1000.0 * 1000.0)) + "us";
+		}else{
+			return String.format("%.3f",(timeSeconds * 1000.0 * 1000.0 * 1000.0))+ "ns";
+		}
 	}
 	
 	public Edge getNextEdge(Node source, Node dest){
